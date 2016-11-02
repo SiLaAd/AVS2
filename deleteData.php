579834d345c809008032c2aa58946074c50d8edb
Adrian <Adrian@Adrian-L440>
2016-11-02 13:03:12 +0100

    [REVERSE] fully functional again

--- deleteData.php
+++ deleteData.php
@@ -39,14 +39,9 @@ function deleteAllFiles() {
     
     while ($file = readdir($path)){
         if($file != "." && $file != ".."){ 
-            $test[] = $file;
-            //unlink($filepath.$file);
+            unlink($filepath.$file);
         }
     } 
-    
-    echo $test[0];
-    echo $test[1];
-    echo $test[2];
     
     echo ("Alle Nutzer wurden gelöscht.");
     closedir($path);  
