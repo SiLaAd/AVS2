bfdfdfdc79a2afd442e85fa16d17e15c17806644
Adrian <Adrian@Adrian-L440>
2016-11-02 11:52:49 +0100

    improved code readability

--- writeData.php
+++ writeData.php
@@ -1,24 +1,13 @@
 <?php
 
 function createFile($username, $ipAdress){
-    
     $filepath = './user/';
     
     if (file_exists ($filepath."$username.txt")){
-        echo("!!! Achtung: Benutzer schon vorhanden !!!");
+        echo("Benutzer schon vorhanden.");
     }else{
-    
-    
-    $datei = fopen($filepath."$username.txt","w");
-    
-    fwrite($datei, "$ipAdress");
-    
-    fclose($datei);
+        $datei = fopen($filepath."$username.txt","w");
+        fwrite($datei, "$ipAdress");
+        fclose($datei);
     }
-    
-      
 }
-
-
-
-?>
