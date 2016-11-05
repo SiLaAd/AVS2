8a0e8e754a831b6d4669b4db33d8db78af02f20c
LarsP <LarsP@Lars.fritz.box>
2016-11-05 15:32:26 +0100

    Abfrage ob user vorhanden + clear table funktioniert

--- requestData.php
+++ requestData.php
@@ -1,21 +1,43 @@
 <?php
 
-$filepath = "./user/"; 
-$path = opendir($filepath); 
+// Auslesen der übertragenen POST-Daten
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+} else {
+    $username = 'username not delivered';
+}
 
-while ($file = readdir($path)){
-        if($file != "." && $file != ".."){ 
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+} else {
+    $password = 'password not delivered';
+}
+
+$filepath = "./user/";
+if (file_exists($filepath . "$username$password.txt")) {
+    requestData();
+} else {
+    echo ("Fehler beim Abrufen der Daten. Sie sind nicht berechtigt.");
+}
+
+function requestData() {
+    $filepath = "./user/";
+    $path = opendir($filepath);
+
+    while ($file = readdir($path)) {
+        if ($file != "." && $file != "..") {
             $fileextensions = array(".", "txt");
             $fileWoEx[] = str_replace($fileextensions, "", $file);
             $files[] = $file;
         }
-    } 
+    }
 
-foreach($files as $file){
-    $content[] = fread(fopen($filepath.$file, 'r'), filesize($filepath.$file));
-}
+    foreach ($files as $file) {
+        $content[] = fread(fopen($filepath . $file, 'r'), filesize($filepath . $file));
+    }
 
-echo json_encode(array(
-    'files'=> $fileWoEx,
-    'content'=> $content
+    echo json_encode(array(
+        'files' => $fileWoEx,
+        'content' => $content
     ));
+}
